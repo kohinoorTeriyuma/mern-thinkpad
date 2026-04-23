c006fd6a0b54b0fe1dc67f3eff07f2243a1e2fd0
naveen seeram <seeramnaveenkumar673@gmail.com>
2026-04-23 22:23:50 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import dns from "dns";
+import path from "path";
 
 import notesRoute from "./routes/notes.route.js";
 import connectDB from "./config/db.js";
@@ -13,8 +14,11 @@ dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
-app.use(cors());
+if (process.env.NODE_ENV !== "production") {
+  app.use(cors());
+}
 app.use(express.json());
 app.use(rateLimiter);
 
@@ -22,6 +26,13 @@ connectDB();
 
 //routes
 app.use("/api/notes", notesRoute);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get(/^(?!\/api).+/, (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log("Server is running on port ", PORT);
